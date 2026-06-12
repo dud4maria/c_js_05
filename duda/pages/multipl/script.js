5fc4d97ad2982d85ced60b80eca82900c4725a65
duda fandangos <dudam4ria.fagundes@gmail.com>
2026-06-12 11:55:34 -0300

    Add files via upload

---
+++ duda/pages/multipl/script.js
@@ -0,0 +1,6 @@
+function mult(){
+    var n1 = parseFloat(document.getElementById('n1').value)
+    var n2 = parseFloat(document.getElementById('n2').value)
+    var res = document.getElementById('res')
+    res.textContent = n1 * n2
+}
---
+++ duda/pages/subtracao/script.js
@@ -0,0 +1,8 @@
+function soma(){
+    var n1 = Number(document.getElementById('n1').value)
+    var n2 = Number(document.getElementById('n2').value)
+    document.getElementById('res').textContent = n1 - n2
+    
+
+}
+
